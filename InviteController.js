636e87d1253edc07fd5064bebe6f156d0c959b04
pjuy12771 <ext.its-group.jimmy.uray@sncf.fr>
2018-08-03 16:27:17 +0200

    crud ready

--- InviteController.js
+++ InviteController.js
@@ -11,16 +11,21 @@ var Invite = require('./InviteObj');
 //The first parameter is the route which will be linked to a function. This function is the second parameter
 router.post('/', function (req, res) {
 
-    Invite.create(
-        {
+    Invite.create({
             name : req.body.name
             //email : req.body.email,
             //password : req.body.password
         }, 
         //fonction de callback (1er parametre : en cas erreur, 2eme paramètre : en cas de succès)
         function (err, invite) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
+            if (err) {
+                return res.status(500).send("There was a problem adding the information to the database.");
+                console.log("erreur creation invite");
+            } else {
+            console.log("un invite : " +invite.name+ " a été ajouté");
             res.status(200).send(invite);
+            }
+            
         }
     );
 
@@ -29,17 +34,74 @@ router.post('/', function (req, res) {
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function (req, res) {
 
-    Invite.find(
-        {
+    Invite.find({
             //Its first parameter, an object, defines the requirements which must be fulfilled in order to return values. 
-            //As in this example the object is empty, all users from the database will be returned
+            //As in this example the object is empty, all invites from the database will be returned
         }, 
             //fonction de callback (1er parametre : en cas erreur, 2eme paramètre : en cas de succès)
-            function (err, invites) {
-            if (err) return res.status(500).send("There was a problem finding the invites.");
-            res.status(200).send(invites);
+        function (err, invites) {
+            if (err) {
+                return res.status(500).send("There was a problem finding the invites.");
+                console.log("erreur recup invites");
+            } else {
+                res.status(200).send(invites);
+                console.log("la liste des invites a été demandée");
+            }
+            
+            
         }
     );
+
+});
+
+// GETS A SINGLE USER FROM THE DATABASE
+router.get('/:id', function (req, res) {
+
+    Invite.findById(req.params.id, function (err, invite) {
+        if (err) {
+            return res.status(500).send("There was a problem finding the invite.");
+            console.log("invite avec id: " +req.param.id+ " introuvable");
+        } 
+        if (!invite) {
+            return res.status(404).send("No invite found.");
+            console.log("aucun invite trouve");
+        } else {
+            res.status(200).send(invite);
+            console.log("invite id : " +invite.id+ " a été trouvé");
+        }
+        
+    });
+
+});
+
+// DELETES A USER FROM THE DATABASE
+router.delete('/:id', function (req, res) {
+
+    Invite.findByIdAndRemove(req.params.id, function (err, invite) {
+        if (err) {
+            return res.status(500).send("There was a problem deleting the invite.");
+            console.log("erreur lors de la suppression de " +invite.name);
+        } else {
+            res.status(200).send("Invite "+ invite.name +" was deleted.");
+            console.log("invite : " +invite.name+ " a été supprimé");
+        }
+    });
+
+});
+
+// UPDATES A SINGLE USER IN THE DATABASE
+router.put('/:id', function (req, res) {
+    
+    Invite.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, invite) {
+        if (err) {
+            return res.status(500).send("There was a problem updating the invite.");
+            console.log("erreur lors de la mise à jour de invite " +invite.name);
+        } else {
+            res.status(200).send(invite);
+            console.log("Mise à jour de invite " +invite.name);
+
+        }
+    });
 
 });
 
--- InviteObj.js
+++ InviteObj.js
@@ -1,11 +1,15 @@
 // declaration de la structure de l'objet invite
 
 var mongoose = require('mongoose');  
-var InviteSchema = new mongoose.Schema({  
-  name: String
-//   email: String,
+var InviteSchema = new mongoose.Schema({
+    name: {type: String, required: true}
+  //status: {type: String, enum: ['todo', 'inProgress', 'done'], default: 'todo', required: true}
+  //   email: String,
 //   password: String
-});
+}, 
+  {timestamps: true} // Pour avoir les dates de création et de modification automatiquement gérés par mongoose
+);
+
 mongoose.model('Invite', InviteSchema);
 
 module.exports = mongoose.model('Invite');
